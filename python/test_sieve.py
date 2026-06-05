a3e81f7a14eb43409ee0cdad33a6b483baa31661
Adrian Hannah <adrian@adrianhannah.net>
2026-06-05 18:14:30 -0400

    fix(python): re-add contract test
     - Re-add test from template with required values to test.

--- python/test_sieve.py
+++ python/test_sieve.py
@@ -32,6 +32,18 @@ class SieveTest(unittest.TestCase):
         for a, b in zip(primes, primes[1:]):
             self.assertLess(a, b, "nth_prime must be strictly increasing")
 
+    def test_sieve_nth_prime(self) -> None:
+        sieve = Sieve()
+        self.assertEqual(2, sieve.nth_prime(0))
+        self.assertEqual(71, sieve.nth_prime(19))
+        self.assertEqual(541, sieve.nth_prime(99))
+        self.assertEqual(3_581, sieve.nth_prime(500))
+        self.assertEqual(7_793, sieve.nth_prime(986))
+        self.assertEqual(17_393, sieve.nth_prime(2_000))
+        self.assertEqual(15_485_867, sieve.nth_prime(1_000_000))
+        self.assertEqual(179_424_691, sieve.nth_prime(10_000_000))
+        # self.assertEqual(2_038_074_751, sieve.nth_prime(100_000_000)) not required, just a fun challenge
+
     def test_sieve_base_case(self) -> None:
         self.assertEqual(2, self.sieve.nth_prime(0))
 
